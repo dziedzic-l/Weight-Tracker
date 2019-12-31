b9d274dbbc17c4bf992e5f6e612a323f6fe0e74d
LDziedzic <pymator@gmail.com>
2019-12-31 09:59:27 +0100

    Add tests in test_models.py

--- measures/tests/test_models.py
+++ measures/tests/test_models.py
@@ -1,9 +1,9 @@
-import datetime
+from datetime import date
 
 from django.contrib.auth.models import User
 from django.test import TestCase
 
-from measures.models import Measure
+from measures.models import Measure, MeasureQuerySet
 
 
 class TestModels(TestCase):
@@ -12,33 +12,37 @@ class TestModels(TestCase):
         Measure.measures.create(
             user=self.user,
             weight=93,
-            date=datetime.date(2019, 12, 30)
+            date=date(2019, 12, 30)
         )
         Measure.measures.create(
             user=self.user,
             weight=93,
-            date=datetime.date(2018, 11, 2)
+            date=date(2018, 11, 2)
         )
         Measure.measures.create(
             user=self.user,
             weight=92,
-            date=datetime.date(2019, 12, 29)
+            date=date(2019, 12, 29)
         )
 
     def test_from_year_method(self):
         measures = Measure.measures.from_year(2019)
         self.assertEquals(measures.count(), 2)
+        self.assertIsInstance(measures, MeasureQuerySet)
 
     def test_from_month_method(self):
         measures = Measure.measures.from_month(12)
         self.assertEquals(measures.count(), 2)
+        self.assertIsInstance(measures, MeasureQuerySet)
 
     def test_get_monthly_averages_of_year(self):
         monthly_avgs = Measure.measures.get_monthly_averages_of_year(2019)
         self.assertEquals(monthly_avgs.count(), 1)
         self.assertTupleEqual(monthly_avgs[0], (12, 92.5))
+        self.assertIsInstance(monthly_avgs, MeasureQuerySet)
 
     def test_get_annual_averages(self):
         annual_avgs = Measure.measures.get_annual_averages()
         self.assertEquals(annual_avgs.count(), 2)
         self.assertTupleEqual(annual_avgs[1], (2019, 92.5))
+        self.assertIsInstance(annual_avgs, MeasureQuerySet)
